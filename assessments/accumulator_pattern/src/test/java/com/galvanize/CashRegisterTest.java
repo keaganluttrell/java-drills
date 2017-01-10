6df6f61eec327f6a5ec8dca704ec115d42c69bde
Brent Gardner <bgardner@squarelabs.net>
2017-01-10 09:47:46 -0700

    Stub out register and test so students understand package locations

---
+++ assessments/accumulator_pattern/src/test/java/com/galvanize/CashRegisterTest.java
@@ -0,0 +1,11 @@
+package com.galvanize;
+
+import com.galvanize.CashRegister;
+import org.junit.Test;
+
+public class CashRegisterTest {
+    @Test
+    public void testRegister() {
+        // Assert something here...
+    }
+}
