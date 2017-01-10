5be360bbafb06549e3486cba98850f2be1fae94a
Brent Gardner <bgardner@squarelabs.net>
2017-01-10 10:50:19 -0700

    Order tests

---
+++ assessments/accumulator_pattern/assessment/test/ProfileTestSuite.java
@@ -0,0 +1,15 @@
+import com.galvanize.NumberListUtilTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                NumberListUtilAssessment.class,
+                StringUtilAssessment.class,
+                HospitalAssessment.class,
+                CashRegisterAssessment.class
+        })
+public class ProfileTestSuite {
+
+}
