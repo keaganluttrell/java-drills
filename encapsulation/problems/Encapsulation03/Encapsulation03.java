6a11ffb49aeb9f022248b50a2f8b1e933e803846
CJ R <hello@cjr.co.de>
2017-02-23 15:27:00 -0700

    :bug: Why is this here!?

--- encapsulation/problems/Encapsulation03/Encapsulation03.java
+++ encapsulation/problems/Encapsulation03/Encapsulation03.java
@@ -5,15 +5,6 @@ import java.util.LinkedList;
 
 class Encapsulation03 {
   public static void main(String[] args){
-
-    LinkedList<String> things = new LinkedList<String>();
-    things.add("Hello");
-    System.out.println(things.get(0));
-
-    Object[] foo = new Object[5];
-    System.out.println("Foo");
-    System.out.println(foo[4]);
-
     Account sourceAccount = new Account();
     Account destinationAccount = new Account();
 
