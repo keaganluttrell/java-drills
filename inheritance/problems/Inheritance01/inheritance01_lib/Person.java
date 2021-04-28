a569b9a949a2cc8bccc27eb7ee9f9f6eef8cb5c4
Keagan Luttrell <keaganluttrell7@gmail.com>
2021-04-28 12:46:59 -0700

    inheritance01 complete

--- inheritance/problems/Inheritance01/inheritance01_lib/Person.java
+++ inheritance/problems/Inheritance01/inheritance01_lib/Person.java
@@ -4,4 +4,12 @@ public class Person {
   public void printName(String first, String last) {
     System.out.println(first + " " + last);
   }
+
+  public void printName(String first, String middle, String last) {
+    System.out.println(first + " " + middle + " " + last);
+  }
+
+  public void printName(Boolean bool, String first, String last) {
+    System.out.printf("%s, %s%n", last, first);
+  }
 }
