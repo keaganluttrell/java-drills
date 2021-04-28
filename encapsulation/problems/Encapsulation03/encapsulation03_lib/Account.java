d4a1fcbed4ace2d496cb38454bfdcff85ca6e342
Keagan Luttrell <keaganluttrell7@gmail.com>
2021-04-28 12:38:21 -0700

    encapsulation03 complete

--- encapsulation/problems/Encapsulation03/encapsulation03_lib/Account.java
+++ encapsulation/problems/Encapsulation03/encapsulation03_lib/Account.java
@@ -2,17 +2,25 @@ package encapsulation03_lib;
 
 public class Account {
   private int balance = 0;
+  private int[] transactions = new int[10];
+  private int index = 0;
 
   public int balance() {
-    return this.balance;
+    int sum = 0;
+    for(int i = 0; i < this.index; i++) {
+      sum += this.transactions[i];
+    }
+    return sum;
   }
 
   public void deposit(int d) {
-    this.balance += d;
+    this.transactions[this.index] = d;
+    this.index++;
   }
 
   public void withdraw(int w) {
-    this.balance -= w;
+    this.transactions[this.index] = w * -1;
+    this.index++;
   }
 
 
